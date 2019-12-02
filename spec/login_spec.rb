d204469eae4bfb009b70dd0ecd878e7a3d686bac
Rodrigo Ramblas <rodrigo_ramblas@hotmail.com>
2019-12-02 14:20:00 -0300

    Foi alterado as aspas

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -1,8 +1,8 @@
 describe 'Login' do
     it 'login' do
         visit 'http://localhost:90/#/login'
-        fill_in "mat-input-0", with: "testes"
-        fill_in "mat-input-1", with: "rbg123456"
-        click_button "Entrar"
+        fill_in 'mat-input-0', with: 'testes'
+        fill_in 'mat-input-1', with: 'rbg123456'
+        click_button 'Entrar'
     end
 end
