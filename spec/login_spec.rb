f9a79410985003dc64b8a65160798e0e7dc2f5bd
Rodrigo Ramblas <rodrigo_ramblas@hotmail.com>
2019-12-02 15:42:05 -0300

    Foi separado o login com sucesso do login sem sucesso

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -1,8 +1,23 @@
-describe 'Login' do
+describe "Login" do
+    it "login de um usuario com sucesso" do
+        visit 'http://localhost:90/#/login'
+        fill_in 'mat-input-0', with: 'testes'
+        fill_in 'mat-input-1', with: 'bgm123456'
+        click_button 'Entrar'
+        @messageok = find('.saludos')
+        sleep 3
+    end
+end
+
+describe "Login sem sucesso" do
     it 'login' do
         visit 'http://localhost:90/#/login'
         fill_in 'mat-input-0', with: 'testes'
-        fill_in 'mat-input-1', with: 'rbg123456'
+        fill_in 'mat-input-1', with: 'bgm126'
         click_button 'Entrar'
+        @messagenok = find('.toast-message')
+        expect(page).to have_no_content('Usuário ou senha Inválidos')
+        sleep 3
+        puts @messagenok
     end
 end
