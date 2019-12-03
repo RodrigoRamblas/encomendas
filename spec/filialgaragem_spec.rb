88d4c1c81942ddd4fcf1b6f11b222fd4427737e5
Rodrigo Ramblas <rodrigo_ramblas@hotmail.com>
2019-12-03 10:48:17 -0300

    Foi add o click no botão ADICIONAR NOTA

--- spec/filialgaragem_spec.rb
+++ spec/filialgaragem_spec.rb
@@ -7,14 +7,12 @@ describe 'Login' do
             fill_in 'mat-input-0', with: 'testes'
             fill_in 'mat-input-1', with: 'bgm123456'
             click_button 'Entrar'
-            @messageok = find('.saludos')
-            sleep 3    
             click_button 'Entrar'
             sleep 5
             fill_in("ex: 97898704021218987040212189870402198704021218", options = {:placeholder => 'ex: 97898704021218987040212189870402198704021218', :with => '35190543648971000155550050006365241050820173'})
-            fill_in("XX/XX/XXXX", options = {:placeholder => 'XX/XX/XXXX', :with => '03/12/2019'})
-            sleep 4
+            fill_in("XX/XX/XXXX", options = {:placeholder => 'XX/XX/XXXX', :with => ' '})
             click_button 'Adicionar nota'
+            sleep 3
         end
     end
 end
