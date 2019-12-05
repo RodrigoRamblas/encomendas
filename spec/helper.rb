adc44359ab936ed5def47ef8c7831f38f78e0b66
Rodrigo Ramblas <rodrigo_ramblas@hotmail.com>
2019-12-05 09:19:57 -0300

    Foi add novos metodos no arquivo helper

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,4 +1,13 @@
 module Helper
+    def login
+        %w(testes).sample
+    end 
+    def notafiscal
+       %w('35190543648971000155550050006365241050820173').sample 
+    end
+    def senha
+        %w(bgm123456).sample
+    end   
     def cpf
         %w(76712477824 82753769800).sample
     end   
@@ -25,5 +34,14 @@ module Helper
     end  
     def remetentecomtabeladefrete
         %w(14912883000110).sample
+    end
+    def quantidade
+        %w(1 2 3 4 5 6 7 8 9).sample
+    end  
+    def peso
+        %w(1 2 3 4 5 6 7 8 9).sample
+    end  
+    def valor
+        %w(100 200 300 400 500 600 700 800 900).sample
     end  
 end
--- spec/sender_without_freight_table_spec.rb
+++ spec/sender_without_freight_table_spec.rb
@@ -8,12 +8,12 @@ describe 'Login' do
     end
     context 'Remetente' do
         it 'Sem tabela de frete' do
-            fill_in 'mat-input-0', with: 'testes' #Usuário
-            fill_in 'mat-input-1', with: 'bgm123456' #Senha
+            fill_in 'mat-input-0', with: login #Usuário
+            fill_in 'mat-input-1', with: senha #Senha
             click_button 'Entrar' #Btn Entrar da tela de Login
             sleep 2
             click_button 'Entrar' #Btn Entrar da tela Selecione Filial/Garagem
-            fill_in("ex: 97898704021218987040212189870402198704021218", options = {:placeholder => 'ex: 97898704021218987040212189870402198704021218', :with => '35190543648971000155550050006365241050820173'}) #NF
+            fill_in("ex: 97898704021218987040212189870402198704021218", options = {:placeholder => 'ex: 97898704021218987040212189870402198704021218', :with => notafiscal}) #NF
             fill_in("XX/XX/XXXX", options = {:placeholder => 'XX/XX/XXXX', :with => ' '}) #Data
             click_button 'Adicionar nota'
             click_button 'ENCOMENDAS'
@@ -30,9 +30,9 @@ describe 'Login' do
             click_button 'Selecione o produto'
             sleep 5
             find('button', text: '(TE1)', exact_text: false).click
-            fill_in 'mat-input-74', with: '1' #Quantidade
-            fill_in 'mat-input-75', with: '1' #Peso
-            fill_in 'mat-input-76', with: '100' #Valor da nota
+            fill_in 'mat-input-74', with: quantidade #Quantidade
+            fill_in 'mat-input-75', with: peso #Peso
+            fill_in 'mat-input-76', with: valor #Valor da nota
             sleep 10
             #ERROcheck 'mat-checkbox-23-input'  #CT-e sem cubagem
             click_button 'Calcular'
