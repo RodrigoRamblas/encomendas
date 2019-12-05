0dd768ee126508be0988da87a7f458654230bf66
Rodrigo Ramblas <rodrigo_ramblas@hotmail.com>
2019-12-05 09:05:15 -0300

    Foi add uma nova funcionalidade para selecionar o produto

--- spec/sender_without_freight_table_spec.rb
+++ spec/sender_without_freight_table_spec.rb
@@ -7,7 +7,7 @@ describe 'Login' do
         visit 'http://localhost:90/#/login'
     end
     context 'Remetente' do
-        xit 'Sem tabela de frete' do
+        it 'Sem tabela de frete' do
             fill_in 'mat-input-0', with: 'testes' #Usuário
             fill_in 'mat-input-1', with: 'bgm123456' #Senha
             click_button 'Entrar' #Btn Entrar da tela de Login
@@ -29,7 +29,7 @@ describe 'Login' do
             click_button 'Mercadoria'
             click_button 'Selecione o produto'
             sleep 5
-            #>>>> Falta implementar a Selecao do produto <<<<
+            find('button', text: '(TE1)', exact_text: false).click
             fill_in 'mat-input-74', with: '1' #Quantidade
             fill_in 'mat-input-75', with: '1' #Peso
             fill_in 'mat-input-76', with: '100' #Valor da nota
@@ -40,11 +40,14 @@ describe 'Login' do
             #Validar se a msg está correta
             expect(find('.c-dialog-content').text).to eql 'Não foi localizada tabela de frete para as condições especificadas.'
         end
-        it 'Com tabela de frete' do
+        xit 'Com tabela de frete' do
             fill_in 'mat-input-0', with: 'testes' #Usuário
             fill_in 'mat-input-1', with: 'bgm123456' #Senha
             click_button 'Entrar' #Btn Entrar da tela de Login
             sleep 2
+            #Falta implementar a pesquisa de garagem
+            #click_button('expand_more')
+            #find('button', text: '30/30', exact_text: false).click
             click_button 'Entrar' #Btn Entrar da tela Selecione Filial/Garagem
             fill_in("ex: 97898704021218987040212189870402198704021218", options = {:placeholder => 'ex: 97898704021218987040212189870402198704021218', :with => '35190543648971000155550050006365241050820173'}) #NF
             fill_in("XX/XX/XXXX", options = {:placeholder => 'XX/XX/XXXX', :with => ' '}) #Data
@@ -60,16 +63,16 @@ describe 'Login' do
             sleep 15
             click_button 'Mercadoria'
             click_button 'Selecione o produto'
-            sleep 5
-            #>>>> Falta implementar a Selecao do produto <<<<
+            sleep 2
+            find('button', text: '(TE1)', exact_text: false).click
             fill_in 'mat-input-74', with: '1' #Quantidade
             fill_in 'mat-input-75', with: '1' #Peso
             fill_in 'mat-input-76', with: '100' #Valor da nota
             sleep 10
             #ERROcheck 'mat-checkbox-23-input'  #CT-e sem cubagem
             click_button 'Calcular'
+            sleep 15
             click_button 'Enviar SEFAZ'
-            sleep 120
         end    
     end
 end
